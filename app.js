c5000192295cb1bb1f5061ddfec4c2c3a8e246e9
mayank-vashishtt <mayankvashishtt@gmail.com>
2024-11-30 02:40:51 +0530

    model,controller,service and route for user and connected db

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ dotenv.config(); // put these two lines at the top of your file
 const express = require('express');
 const cors  = require('cors');
 const app = express();
+const connectToDB = require('./db/db');
+const userRoutes = require('./routes/user.routes');
 
+
+
+connectToDB();
 app.use(cors()); // read about this 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.use('/users', userRoutes);   
 
 module.exports = app;
 
---
+++ models/user.model.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { use } = require('../app');
+
+
+
+const userSchema = new mongoose.Schema({
+
+    fullName: {
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: [3, "firstName must be at least 3 characters"],
+      
+        },
+        lastName: {
+            type: String,
+            trim: true,
+            minlength: [3, "lastName must be at least 3 characters"],
+      
+        }
+       
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minlength: [5, "email must be at least 5 characters"],
+      
+    },
+
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+
+    socketID:{
+        type: String,
+
+    } // for live tracking of the uber driver 
+
+
+
+
+})
+//14:00 
+// bcrypt -- for hashing passwords and comparing hashed passwords
+// jwt -- for generating tokens
+
+userSchema.methods.generateAuthToken = async function () {
+
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+    return token ;
+}
+
+userSchema.methods.comparePassword = async function (password) {
+
+    return await bcrypt.compare(password, this.password);
+}
+
+
+userSchema.statics.hashPassword = async function (password) {
+
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
+
